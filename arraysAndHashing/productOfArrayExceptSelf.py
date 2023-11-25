9aec2308d17f4f732e389fb1cf75bf230a37a73e
Y-Noor <59338864+Y-Noor@users.noreply.github.com>
2023-11-25 21:03:39 +0400

    Update productOfArrayExceptSelf.py

--- arraysAndHashing/productOfArrayExceptSelf.py
+++ arraysAndHashing/productOfArrayExceptSelf.py
@@ -1,12 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        x = 0
-        for curr in range(len(nums)):
-            for multiplier in range(len(nums)):
-                if multiplier == curr:
-                    pass
-                else:
-                    nums[curr] = nums[curr] * nums[multiplier] 
-            x += 1
-        return nums
+        results = [1] * len(nums)
+        
+        prefix = 1
+        for i in range(len(nums)):
+            results[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            results[i] *= postfix
+            postfix *= nums[i]
+        
+        return results
