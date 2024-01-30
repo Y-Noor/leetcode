c41505e4cda2895f5c012f924e405828e44ac527
Y-Noor <59338864+Y-Noor@users.noreply.github.com>
2024-01-30 23:41:10 +0400

    Update 115validpalindrome.py

--- 115validpalindrome.py
+++ 115validpalindrome.py
@@ -15,4 +15,14 @@ class Solution:
                 return False
         return True
 
+
+'''
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ''
+        for a in s:
+            if a.isalpha() or a.isdigit():
+                new += a.lower()
+        return (new == new[::-1])
+'''
         
